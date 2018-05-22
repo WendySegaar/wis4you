54f550010d3ed7add40ccce69db17aae8a53cd1f
WendySegaar <36224315+WendySegaar@users.noreply.github.com>
2018-05-22 13:14:31 +0200

    rekenpagina
    rekenpagina begin

---
+++ js/berekening.js
@@ -0,0 +1,9 @@
+function () {}
+
+
+Maak een keuze welke bereking je uit wilt voeren
+
+
+<input type="text" id="input1">
+		<input type="text" id="input2">
+		<input type="submit" onclick="startBereken()">

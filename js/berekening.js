50d1715ddb0f90a0249d5950372af278fb16faf5
jarcosimons <jarco.simons@gmail.com>
2018-05-28 19:32:42 +0200

    breuken reeks toegevoegd
    breuken reeks toegevoegd

--- js/berekening.js
+++ js/berekening.js
@@ -44,6 +44,17 @@ function macht(){
 		}
 	}
 }
+function breuk() {
+	var breukGetal = document.getElementById('breukGetal').value;
+	var uitkomst = 0 ;
+
+	if (parseInt(breukGetal)) {
+		for (var i = 1; i <= breukGetal; i++) {
+			uitkomst = 1 / i ;
+			document.getElementById('uitkomst').innerHTML += "<br/> 1/" + i + " = " + uitkomst ;
+		}
+	}
+}
 function kwadraat() {
 	var kwadraatGetal = document.getElementById('kwadraatGetal').value;
 	var uitkomst = 0 ;

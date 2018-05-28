58852cc245c8b422adbb01fdcce6c43e0b180024
WendySegaar <36224315+WendySegaar@users.noreply.github.com>
2018-05-28 11:32:00 +0200

    js tafelreeks en knopjes
    Js tafelreeks en knopjes geschreven

--- js/berekening.js
+++ js/berekening.js
@@ -1,9 +1,30 @@
 
 function bereken(x) {
-	if(x = 1){
-		document.getElementById("berekening").innerHTML = "Uitleg<input type='text' id='input1'><input type="text" id="input2"><input type="submit" onclick="startBereken()">";
+	if(x == 1){
+		document.getElementById("berekening").innerHTML = "Uitleg Tafelreeks<br/><input type='text' id='tafelGetal'><br/><input type='text' id='tafelAantal'><input type='submit' onclick='tafel();'>";
+ 	}
+ 	else if(x == 2){
+ 		document.getElementById("berekening").innerHTML = "Uitleg Machtreeks<br/><input type='text' id='tafelGetal'><br/><input type='text' id='tafelAantal'><input type='submit' onclick='macht();'>";
  	}
 }
 
 
 
+function tafel(){
+	var tafelGetal = document.getElementById('tafelGetal').value ;
+	var tafelAantal = document.getElementById('tafelAantal').value ;
+	var uitkomst = 0 ;
+
+	if (parseInt(tafelGetal)){
+		if (parseInt(tafelAantal)) {
+			for (var i=0; i <= tafelAantal; i++) {
+				uitkomst = i * tafelGetal;
+				document.getElementById('uitkomst').innerHTML += "<br/>" + i+ " x " +tafelGetal+ " = " +uitkomst ;
+			}
+		}
+		else{alert("Vul een geheel getal in voor het aantal van de reeks");}
+	}
+	else{alert("Vul een geheel getal in voor welke tafel je wilt berekenen");}
+}
+
+function macht()

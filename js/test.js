45f27d686fe993377f6d5b3d5f9662da11a02a7a
jarcosimons <jarco.simons@gmail.com>
2018-06-04 15:09:03 +0200

    priem getallen function
    priem getallen function

--- js/test.js
+++ js/test.js
@@ -2,14 +2,17 @@ function bereken(x) {
 	if(x == 5){
 		document.getElementById('berekening').innerHTML = "Uitleg fibonaccireeks<p>Deze fibonaccireeks begint altijd met 0 en 1 en de volgende uitkomst is de laatste 2 uitkomsten bij elkaar op geteld, en het doet dat tot het getal dat je heb ingevult.</p><label for='fibonacciGetal'>Tot:</label><input class='inputRek' type='text' id='fibonacciGetal'><br/><input id='submitRek' type='submit' onclick='fibonacci();'>";
 	}
+	if(x == 6){
+		document.getElementById('berekening').innerHTML = "Uitleg Priem getallen reeks<p>De de priem getallen reeks laat alle getallen zien die nergens deelbaar door is behalven door 1 en zichzelf.</p><label for='priemGetal'>Tot:</label><input class='inputRek' type='text' id='priemGetal'><br/><input id='submitRek' type='submit' onclick='priemGetal();'>";
+	}
 }
 
 
 function fibonacci() {
 	var fibonacciGetal = document.getElementById('fibonacciGetal').value ;
-	var a = 0
-	var b = 1
-	var c = 0
+	var a = 0 ;
+	var b = 1 ;
+	var c = 0 ;
 
 	if (parseInt(fibonacciGetal)) {
 		for (var i = 0; i < fibonacciGetal; i = c) {
@@ -18,5 +21,16 @@ function fibonacci() {
 			a = b ;
 			b = c ;
 		}
+	}
+}
+function priemGetal() {
+	var priemGetal = document.getElementById('priemGetal').value ;
+	var p(0) = 2 ;
+	var p(1), test = 3 ;
+	var teller = 2; 
+
+	if (parseInt(priemGetal)) {
+		for (var i = 0 ; i < priemGetal; i = test) {
+			
 	}
 }

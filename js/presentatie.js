a937ade7fbaa5edc464239fd8d8cf3f19ebc2092
jarcosimons <jarco.simons@gmail.com>
2018-06-18 11:19:09 +0200

    presentatie toegevoegt
    ik heb de presentatie werkend gekregen met 2 knoppen en ik ga het nu probeeren met keypress

---
+++ js/presentatie.js
@@ -0,0 +1,28 @@
+var arr;
+var counter = 0;
+
+function SetupArray() {
+	arr = new Array("een", "twee", "drie");
+	document.getElementById('dia').innerHTML = "<img id='presentatieFoto' src='images/" + arr[counter] + ".jpg'>";
+	document.getElementById('presentatieFoto').style.width = "100%";
+}
+function move(x){
+	counter = counter + x;
+	if  (counter == -1){
+		counter = 2;
+		document.getElementById('dia').innerHTML = "<img id='presentatieFoto' src='images/" + arr[counter] + ".jpg'>";
+		document.getElementById('presentatieFoto').style.width = "100%";
+
+	}
+	else if (counter == 3){
+		counter = 0;
+		document.getElementById('dia').innerHTML = "<img id='presentatieFoto' src='images/" + arr[counter] + ".jpg'>";
+		document.getElementById('presentatieFoto').style.width = "100%";
+
+	}
+	else{
+		document.getElementById('dia').innerHTML = "<img id='presentatieFoto' src='images/" + arr[counter] + ".jpg'>";
+		document.getElementById('presentatieFoto').style.width = "100%";
+
+	}
+}

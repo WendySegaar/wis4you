ea903f8bbca786cc901bdc0c4e1ca2a1038c912c
WendySegaar <36224315+WendySegaar@users.noreply.github.com>
2018-06-28 09:31:32 +0200

    pijltjestoetsen
    Slideshow is te bedienen met pijltjes

--- js/presentatie.js
+++ js/presentatie.js
@@ -1,32 +1,26 @@
 var arr;
 var counter = 0;
+var x;
 
 window.onload = function SetupArray() {
 	arr = new Array("slide1.png", "slide2.png", "slide3.png", "slide4.png", "slide5.png", "slide6.png", "slide7.png", "slide8.png", "slide9.png");
-	document.getElementById('dia').innerHTML = "<img id='presentatieFoto' src='images/" + arr[counter] + "/>";
+	document.getElementById('dia').innerHTML = "<img id='presentatieFoto' src='images/" + arr[counter] + "'/>";
 	document.getElementById('presentatieFoto').style.width = "100%";
 }
+window.addEventListener('keydown', function(e){
+	if(e.keyCode == 39){ x=1;}
+	else if(e.keyCode == 37){ x=-1;}
+	move(x);
+})
 function move(x){
 	counter = counter + x;
 	if  (counter <= -1){
 		counter = 8;
-		document.getElementById('dia').innerHTML = "<img id='presentatieFoto' src='images/" + arr[counter] + "/>";
-		document.getElementById('presentatieFoto').style.width = "100%";
-
 	}
-	else if (counter == 9){
+	else if (counter >= 9){
 		counter = 0;
-		document.getElementById('dia').innerHTML = "<img id='presentatieFoto' src='images/" + arr[counter] + "/>";
-		document.getElementById('presentatieFoto').style.width = "100%";
-
 	}
-	else{
-		document.getElementById('dia').innerHTML = "<img id='presentatieFoto' src='images/" + arr[counter] + "/>";
-		document.getElementById('presentatieFoto').style.width = "100%";
-
-	}
+	
+	document.getElementById('dia').innerHTML = "<img id='presentatieFoto' src='images/" + arr[counter] + "'/>";
+	document.getElementById('presentatieFoto').style.width = "100%";
 }
-// window.addEventListener('keydown', function(e){
-// 	if(e.keyCode == 39){ counter++;}
-// 	else if(e.keyCode == 37){ counter--;}
-// })
